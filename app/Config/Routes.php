a7ecf5acbd9c444c203c6331e6598f6f4b6661a0
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-04-11 15:57:29 +0300

    Feat : List equipes in a given tournament

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,11 @@ $routes->group('api', function($routes)
 {
     $routes->resource('utilisateur', ['controller' => 'UtilisateurJController']);
     $routes->resource('discipline', ['controller' => 'DisciplineJController']);
+
+    // Listes des équipes participant à un tournoi
+    $routes->get('equipes/tournoi/(:num)', 'EquipeController::tournoi/$1');
+    $routes->get('equipes/tournoi', 'EquipeController::tournoi');
+
 });
+
 // $routes->get('api/utilisateur', 'UtilisateurJController::index');
