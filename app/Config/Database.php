d15ae77b28acdb70a1c85a3c8ed05e2cb390685a
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2024-04-11 18:04:22 +0300

    config [DB] change database.php to common username and pwd

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -79,8 +79,8 @@ class Database extends Config
        public array $default = [
            'DSN'        => '',
            'hostname'   => 'localhost',
-           'username'   => 'postgres',
-           'password'   => 'root',
+           'username'   => 'madaebvx_ituser',
+           'password'   => '{wlks7jdg^Pr',
            'database'   => 'madaebvx_itusport',
            'schema'     => 'public',
            'DBDriver'   => 'Postgre',
