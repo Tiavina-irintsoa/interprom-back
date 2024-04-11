6794f069fdea6e7661c05ef1a7e7aa3a9e3c65bf
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-04-11 16:37:52 +0300

    feat back-end: liste simple

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,10 @@ $routes->group('api', function($routes)
 {
     $routes->resource('utilisateur', ['controller' => 'UtilisateurJController']);
     $routes->resource('discipline', ['controller' => 'DisciplineJController']);
+    $routes->resource('poule', ['controller' => 'PouleJController']);
 });
 // $routes->get('api/utilisateur', 'UtilisateurJController::index');
+
+
+$routes->get('api/match-en-cours', 'MatchJController::get_matche_en_cours_par_discipline');
+$routes->get('api/match-a-suivre', 'MatchJController::get_matche_a_suivre');
