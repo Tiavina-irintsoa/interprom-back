e9e2163df137826603b49e9dffa41bd3ce8531f6
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-04-12 08:27:31 +0300

    Fix : Update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,7 +18,6 @@ $routes->group('api', function($routes)
 });
 
 $routes->post('login','LoginController::index');
-$routes->post('/api/matchs/update_score', 'MatchController::update_score');
 
 // Routes à propos des équipes
 $routes->get('api/equipes/tournoi/(:num)', 'EquipeController::tournoi/$1');
@@ -29,18 +28,10 @@ $routes->get('matchs/all/(:num)/(:num)', 'MatchController::list_match_by_discipl
 $routes->get('matchs/(:num)/start', 'MatchController::start_match/$1');
 $routes->get('matchs/(:num)/end', 'MatchController::end_match/$1');
 
-$routes->post('api/matchs/update_score', 'MatchController::update_score');
 $routes->get('api/match-en-cours', 'MatchJController::get_matche_en_cours_par_discipline');
 $routes->get('api/match-a-suivre', 'MatchJController::get_matche_a_suivre');
-<<<<<<< Updated upstream
-=======
 $routes->post('api/matchs/update_score', 'MatchController::update_score');
 
-
-$routes->get('api/match-en-cours', 'MatchJController::get_matche_en_cours_par_discipline');
-$routes->get('api/match-a-suivre', 'MatchJController::get_matche_a_suivre');
->>>>>>> Stashed changes
 $routes->get('api/match-en-cours/(:num)', 'MatchJController::get_matche_en_cours_par_discipline/$1');
-$routes->get('api/match-a-suivre', 'MatchJController::get_matche_a_suivre');
 $routes->get('api/statistique-par-poule/(:num)', 'PouleJController::get_resultat_poule_choisie/$1');
 $routes->get('api/classement-par-poule/(:num)', 'PouleJController::get_classement_par_poule_choisi/$1');
