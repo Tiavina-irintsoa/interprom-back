ecfe88a7088e0de76d5eaf98bb0ff19bf47812f7
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-04-12 09:03:58 +0300

    Fix : Ajout discipline dans les équipes jouant a un tournoi

--- app/Controllers/EquipeController.php
+++ app/Controllers/EquipeController.php
@@ -12,7 +12,7 @@ class EquipeController extends ResourceController
     public function tournoi($id_tournoi = 1)
     {
         $equipe_tournoi = new VEquipeTournoiLibCompModel();
-        $equipe_tournoi->select('id_equipe, nom_equipe, nom_poule');
+        $equipe_tournoi->select('id_equipe, nom_equipe, nom_poule, nom_discipline');
         $equipes = $equipe_tournoi->where('id_tournoi', $id_tournoi)->findAll();
 
         $data = [
