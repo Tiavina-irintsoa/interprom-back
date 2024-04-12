fe4b4d51ff3158ca5346b1ddadd9e7a79d06475b
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-04-12 09:23:04 +0300

    Fix : Format response data

--- app/Controllers/MatchController.php
+++ app/Controllers/MatchController.php
@@ -81,7 +81,8 @@ class MatchController extends ResourceController{
 
         $data = [
             'status' => 1,
-            'data' => $matchs
+            'data' => $matchs,
+            'null' => null
         ];
 
         return $this->respond($data);
@@ -128,7 +129,8 @@ class MatchController extends ResourceController{
         if (isset($match['debut_reel'])) {
             return $this->respond([
                 'status' => 0,
-                'error' => "Le match que vous voulez spécifier a déja commencé !"
+                'error' => "Le match que vous voulez spécifier a déja commencé !",
+                'data' => null
             ]);
         }
 
@@ -142,7 +144,8 @@ class MatchController extends ResourceController{
 
         return $this->respond([
             'status' => 1,
-            'data' => 'Match commencé avec success !'
+            'data' => 'Match commencé avec success !',
+            'error' => null
         ]);
     }
 
@@ -154,14 +157,16 @@ class MatchController extends ResourceController{
         if (!isset($match)) {
             return $this->respond([
                 'status' => 0,
-                'error' => "Le match que vous voulez terminer n' éxiste même pas"
+                'error' => "Le match que vous voulez terminer n' éxiste même pas",
+                'data' => null
             ]);
         }
 
         if (isset($match['fin_reel'])) {
             return $this->respond([
                 'status' => 0,
-                'error' => "Le match que vous voulez spécifié est déja terminer !"
+                'error' => "Le match que vous voulez spécifié est déja terminer !",
+                'data' => null
             ]);
         }
 
@@ -175,7 +180,8 @@ class MatchController extends ResourceController{
 
         return $this->respond([
             'status' => 1,
-            'data' => 'Match terminé avec success !'
+            'data' => 'Match terminé avec success !',
+            'error' => null
         ]);
     }
 }
