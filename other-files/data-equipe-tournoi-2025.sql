2f742500299a47c90ebd4b99078b9d0efd38ee86
Mialy05 <mialisoamurielle@gmail.com>
2025-05-16 00:29:25 +0300

    fix [CLASSEMENT] corrgier la vue pour récupérer le classement par équipe par poule

---
+++ other-files/data-equipe-tournoi-2025.sql
@@ -0,0 +1,8 @@
+INSERT INTO equipe_tournoi 
+(id_equipe, id_tournoi, id_poule, code_equipe)
+VALUES
+-- Volley Groupe A
+( 20, 2, 3, 'A21'),
+( 21, 2, 3, 'A22'),
+( 22, 2, 3, 'A23'),
+( 23, 2, 3, 'A24');
